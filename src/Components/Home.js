23c011aa9b6124be35afbe78c0d376781f8e02a4
chetan joshi <chtnjsh7@gmail.com>
2025-02-05 17:25:24 +0530

    card_content

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,6 @@
 import Navbar from "./Navbar";
 import "./styles/home.css";
+import stars from "../Assets/Orion constellation2.jpg"
 import Footer from "./Footer"
 
 const Home = () => {
@@ -7,12 +8,11 @@ const Home = () => {
     <>
       <Navbar />
       <div className="main_wraphome col-md-12 col-ld-12 col-sd-12">
-        <div className="card home_card col-md-6 col-sd-12 col-ld-6">
-          {/* <img src="..." className="card-img-top" alt="..."> */}
-          <div className="card-body">
+        <div className="card home_card card_bg col-md-6 col-sd-12 col-ld-6">
+          {/* <img src={stars} className="card-img-top" alt="..."/> */}
+          <div className="card-body ">
             <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+             Match Making
             </p>
           </div>
         </div>
